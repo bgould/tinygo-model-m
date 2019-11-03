e98b5d632c58e1ea434fa908bc6f9e57ea5892f3
BCG <bgould@users.noreply.github.com>
2019-11-03 12:15:08 -0500

    tweaked formatting of keymap

--- modelm/keymap.go
+++ modelm/keymap.go
@@ -2,7 +2,7 @@ package modelm
 
 import (
 	"github.com/bgould/tinygo-model-m/keyboard"
-	
+
 	. "github.com/bgould/tinygo-model-m/keyboard/keycodes"
 )
 
@@ -11,8 +11,8 @@ func ANSI101DefaultLayer() keyboard.Keymap {
 
 	ESC,      F1,  F2,  F3,  F4,    F5,  F6,  F7,  F8,    F9,  F10, F11, F12,   PSCR,SLCK,BRK,
 
-	GRV, N1,  N2,  N3,  N4,  N5,  N6,  N7,  N8,  N9,  N0,  MINS,EQL, BSPC,  INS, HOME,PGUP,  NLCK,PSLS,PAST,PMNS,
-	TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,  DEL, END, PGDN,  P7,  P8,  P9,  PPLS,
+  GRV, N1,  N2,  N3,  N4,  N5,  N6,  N7,  N8,  N9,  N0,  MINS,EQL, BSPC,  INS, HOME,PGUP,  NLCK,PSLS,PAST,PMNS,
+  TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,  DEL, END, PGDN,  P7,  P8,  P9,  PPLS,
 	CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                    P4,  P5,  P6,
 	LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,       UP,         P1,  P2,  P3,  PENT,
 	LCTL,LALT,                  SPC,                           RALT, RCTL,  LEFT,DOWN,RGHT,  P0,       PDOT,
@@ -22,13 +22,13 @@ func ANSI101DefaultLayer() keyboard.Keymap {
 
 func ANSI101Keymap(
 
-  K72,      K53, K54, K64, K74,   K76, K78, K69, K59,   K56, K46, K4B, K4C,   K4F, K3F, K1E, 
-    
-  K52, K42, K43, K44, K45, K55, K57, K47, K48, K49, K4A, K5A, K58, K66,   K5C, K5E, K5D,   K1B, K1C, K1D, K0D, 
-  K62, K32, K33, K34, K35, K65, K67, K37, K38, K39, K3A, K6A, K68, K26,   K5B, K4E, K4D,   K3B, K3C, K3D, K3E, 
-  K63, K22, K23, K24, K25, K75, K77, K27, K28, K29, K2A, K7A, K16,                         K6B, K6C, K6D, 
-  K61, K12, K13, K14, K15, K05, K07, K17, K18, K19, K0A, K11,                  K7E,        K2B, K2C, K2D, K2E, 
-  K50, K7F, K06, K0F, K10,                                                K0E, K0B, K0C,   K7C,      K7D       Keycode,
+  K72,      K53, K54, K64, K74,   K76, K78, K69, K59,   K56, K46, K4B, K4C,   K4F, K3F, K1E,
+
+  K52, K42, K43, K44, K45, K55, K57, K47, K48, K49, K4A, K5A, K58, K66,   K5C, K5E, K5D,   K1B, K1C, K1D, K0D,
+  K62, K32, K33, K34, K35, K65, K67, K37, K38, K39, K3A, K6A, K68, K26,   K5B, K4E, K4D,   K3B, K3C, K3D, K3E,
+  K63, K22, K23, K24, K25, K75, K77, K27, K28, K29, K2A, K7A,      K16,                    K6B, K6C, K6D,
+  K61, K12, K13, K14, K15, K05, K07, K17, K18, K19, K0A,           K11,        K7E,        K2B, K2C, K2D, K2E,
+  K50, K7F,                   K06,                            K0F, K10,   K0E, K0B, K0C,   K7C,      K7D       Keycode,
 
 ) keyboard.Keymap {
 	return keyboard.Keymap([8][16]Keycode{
